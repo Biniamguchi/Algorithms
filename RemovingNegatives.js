dc386445a1508cc80a62272797f1bcd8082c7388
Biniam <biniam22@gmail.com>
2018-02-04 01:09:02 -0500

    Removing Negatives

--- RemovingNegatives.js
+++ RemovingNegatives.js
@@ -1,10 +1,17 @@
 function removeNeg(){
     var arr = [0, -1, 2, -3, 4, -5, 6];
-    for (var i = 0; i < arr.length; i++){
-        if (arr[i] < 0){
-            arr.pop(arr[i])
+    var countNeg = 0;
+    for (var i = 1; i < arr.length; i++){
+         if (arr[i] < 0){
+           countNeg++;              
+        }
+         else {
+           arr[i-countNeg] = arr[i];     
         }
     }
+    while (countNeg--){
+      arr.pop();
+      }
     console.log(arr);
 }
 removeNeg()
