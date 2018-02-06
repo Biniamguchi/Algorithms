d8c11ce974d3b1cc0b7f0487147f2c4472604431
Biniam <biniam22@gmail.com>
2018-02-06 00:12:24 -0500

    Algorithm assignments

--- PrintSumofOdds1To5000.js
+++ PrintSumofOdds1To5000.js
@@ -7,3 +7,18 @@ function printSumofOdds1To5000() {
 }
 
 printSumofOdds1To5000()
+
+or 
+
+function printSumofOdds1To5000() {
+    var sum = 0;
+    for (var i = 1; i <= 5000; i++) {
+        if (i%2===1){
+            sum += i;
+        }        
+        
+    }
+    console.log(sum);
+}
+
+printSumofOdds1To5000()
