b4712757990e69341c5c257429270b4eca3ac6c7
Biniam <biniam22@gmail.com>
2018-02-06 00:08:14 -0500

    Iterate an array

---
+++ IterateArray.js
@@ -0,0 +1,13 @@
+function iterateArr(){
+  var arr = [1,2,5];
+  var sum = 0;
+  for (var i = 0; i < arr.length; i++){
+    sum += arr[i];
+    
+    }
+  console.log(sum);
+  }
+  iterateArr()
+
+  // Write a function that returns the sum of all the values within an array. 
+  // (e.g. [1,2,5] returns 8, [-5,2,5,12] returns 14)
