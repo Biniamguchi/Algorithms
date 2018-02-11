3cc918007cba2254f2b5234360d1b1401a49b4f4
Biniam <biniam22@gmail.com>
2018-02-10 23:50:35 -0500

    Iterate Array

--- IterateArray.js
+++ IterateArray.js
@@ -10,4 +10,4 @@ function iterateArr(){
   iterateArr()
 
   // Write a function that returns the sum of all the values within an array. 
-  // (e.g. [1,2,5] returns 8, [-5,2,5,12] returns 14)
+  // (e.g. [1,2,5] returns 8, [-5,2,5,12] returns 14).
