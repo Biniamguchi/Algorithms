be21cc7632ef8726ec51140bbc7f4a94ca09f10a
Biniam <biniam22@gmail.com>
2018-02-12 23:01:20 -0500

    for loops

---
+++ TwoForLoops.js
@@ -0,0 +1,14 @@
+var msg1 ='hello';
+var msg2 = 'world!';
+
+for (var num1=1; num1<=msg1.length; num1++){
+    for (var num2=1; num2<=msg2.length; num2++){
+        if (msg1.lenth ==4){
+            cosole.log(msg1 +''+msg2);
+        }
+    }
+
+    if(num1 !=4){
+        console.log(msg1.length+msg2.length);
+    }
+}
